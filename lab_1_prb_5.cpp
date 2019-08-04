966fcb78fac57261ea8c9ddd4f7f2cbaeb3974ea
Munindra Naik <50791550+munindra2132@users.noreply.github.com>
2019-08-04 23:55:58 +0530

    Add files via upload

---
+++ lab_1_prb_5.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+
+int main()
+{ int cas;
+ cout<<"Enter number of case: ";
+ cin>>cas;
+ for(int i=0;i<cas<i++)
+ { 
+   int suc=0;
+   string p;
+   cout<<"Enter number: "
+   cin>>p;
+   for(int i=1;i< sqrt(stoi(p));i++)
+    {
+      if(stoi(p)%i==0) 
+       {suc++;
+         break;
+        }
+  if(suc!=0)
+   cout<<"Not prime";
+  else
+   cout<<"Prime";
+  }
+return 0;
+}
